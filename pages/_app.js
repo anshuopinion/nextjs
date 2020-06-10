2a4b8f3fa1fa83d0d1ff58f3a10fb5c7c2fcbf68
anshuopinion <anshusopinion@gmail.com>
2020-06-10 21:38:02 +0530

    appp

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import Header from '../components/Header';
 import '../sass/main.scss';
 import Router from 'next/router';
-// import NProgress from 'nprogress'; //nprogress module
-// import 'nprogress/nprogress.css'; //styles of nprogress//Binding events.
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress//Binding events.
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Footer from '../components/Footer/Footer';
-// NProgress.configure({ showSpinner: false });
-// Router.events.on('routeChangeStart', () => NProgress.start());
-// Router.events.on('routeChangeComplete', () => NProgress.done());
-// Router.events.on('routeChangeError', () => NProgress.done());
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps, navigations }) => {
   return (
